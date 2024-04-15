e9ed0d80d435dea13b952d285b7bd2de152096ab
Eduardo Barbosa <eduardobarbosalhp@gmail.com>
2024-04-14 21:22:25 -0300

    configuracao banco de dados

---
+++ app/src/main/java/com/edubarbosa/carteiradeclientes/database/DadosOpenHelper.java
@@ -0,0 +1,25 @@
+package com.edubarbosa.carteiradeclientes.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DadosOpenHelper extends SQLiteOpenHelper {
+    public DadosOpenHelper(Context context) {
+        super(context, "DADOS", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(ScriptDLL.getCreateTableCliente());
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
