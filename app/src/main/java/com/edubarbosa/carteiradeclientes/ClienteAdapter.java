e1b8bb4a0e4ae88ee96fa478276e80cf1f46ef0b
Eduardo Barbosa <eduardobarbosalhp@gmail.com>
2024-04-15 22:03:02 -0300

    inicio setup recycler view e propriedades

--- app/src/main/java/com/edubarbosa/carteiradeclientes/ClienteAdapter.java
+++ app/src/main/java/com/edubarbosa/carteiradeclientes/ClienteAdapter.java
@@ -1,24 +1,51 @@
 package com.edubarbosa.carteiradeclientes;
 
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ClienteAdapter extends RecyclerView.Adapter {
+import com.edubarbosa.carteiradeclientes.dominio.entidades.Cliente;
+
+import java.util.List;
+
+public class ClienteAdapter extends RecyclerView.Adapter<ClienteAdapter.ViewHolderCliente> {
+
+    //private AdapterClienteBinding binding;
+    private List<Cliente> dados;
+
+    public ClienteAdapter(List<Cliente> listaClientes) {
+        dados = listaClientes;
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ClienteAdapter.ViewHolderCliente onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ClienteAdapter.ViewHolderCliente holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dados.size();
+    }
+
+    public class ViewHolderCliente extends RecyclerView.ViewHolder {
+        public TextView tvNome;
+        public TextView tvTelefone;
+
+        public ViewHolderCliente(@NonNull View itemView) {
+            super(itemView);
+
+            tvNome = itemView.findViewById(R.id.tv_nome);
+            tvTelefone = itemView.findViewById(R.id.tv_telefone);
+
+        }
     }
 }
